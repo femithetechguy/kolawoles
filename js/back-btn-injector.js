960f989deff65926ba6f7e9d20e3adb4b0dd4c8f
Tech Guy <femithetechguy@gmail.com>
2025-08-18 05:25:42 -0400

    remved mobile naigation

--- js/back-btn-injector.js
+++ js/back-btn-injector.js
@@ -1,12 +1,2 @@
-// This script is intended to be injected into destination pages
-// It checks if the page was opened from Kolawoles.com and adds a back button if so
-(function() {
-  // Check if we have a stored script from Kolawoles.com
-  const backBtnScript = localStorage.getItem('kolawoles-back-btn-script');
-  if (backBtnScript) {
-    // Execute the script
-    eval(backBtnScript);
-    // Clear it so it doesn't affect other sites
-    localStorage.removeItem('kolawoles-back-btn-script');
-  }
-})();
+// Back button injector functionality removed as requested
+// This file is kept as a placeholder in case this functionality is needed in the future
